92595cce1413e03e395d41cfd07cc67280ed264e
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-07-15 15:10:06 -0300

    first commit

---
+++ src/test/java/com/ewecarreira/spring/boot/expert/StartupTests.java
@@ -0,0 +1,13 @@
+package com.ewecarreira.spring.boot.expert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
